a0b3262ac30b75c4e8cc089bfa7f7b0f25e2e2ee
Lazare Gogoladze <lazaregogoladzedev@gmail.com>
2025-03-31 13:51:23 +0400

    four

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/LeriGogoladze/puppy
 
 go 1.23.1
+
+require github.com/LeriGogoladze/dog v0.0.0-20250331094213-824999719b79
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/LeriGogoladze/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof Woof!!"
+}
+
+func BigBark() string {
+	return dog.WhenGrowingUp("Bark()")
+}
+
+func BigBarks() string {
+	return dog.WhenGrowingUp("Barks()")
 }
